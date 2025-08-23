00ffc3220fd23caf411103b2557480dd7a78204c
Shagor "Shaggy" Rahman <42647364+Shaggstar@users.noreply.github.com>
2025-08-23 14:29:13 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: '/exhibitingbias-site/' // replace with '/<repo-name>/' if different
+  base: '/exhibitingBias/',   // 👈 add this line
 })
