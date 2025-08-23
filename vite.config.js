2ed17dc410441d09ef8b3237eeb050dff5f298c0
Shagor "Shaggy" Rahman <42647364+Shaggstar@users.noreply.github.com>
2025-08-23 15:03:00 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/exhibitingBias/',  
-  plugins: [react()]
+  plugins: [react()],
+  base: '/exhibitingBias/'   
 })
